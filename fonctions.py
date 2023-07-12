ebab1579f39f8762062b04ac9b697cb8ffb34a7e
DESKTOP-C03PQIV\pierr <pierre.ghesquiere@lilo.org>
2023-07-12 13:02:23 +0200

    no message

--- fonctions.py
+++ fonctions.py
@@ -10,8 +10,7 @@ def voirSignaux(ondes):
     for i in range(nbondes):
         onde = ondes[i]
         retard = max(retard,onde.retard)
-        
-    plt.text(0,2.1,"retard = "+str(round(retard,2))+' s',size=12,
+    plt.text(0,2.1,"retard = "+str(round(retard,6))+' s',size=12,
         bbox =dict(boxstyle="round",ec=("k"),fc=("c")))
     for i in range(nbondes):
         plt.plot(ondes[i].temps,ondes[i].signal,couleurs[i], label = labels[i])
